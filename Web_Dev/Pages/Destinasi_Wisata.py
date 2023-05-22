fb67f4e41544301ef0d106dc734dd98d8a26d977
Devinchand <100545304+Devinchand@users.noreply.github.com>
2023-05-22 22:59:32 +0700

    Add files via upload

---
+++ Web_Dev/Pages/Destinasi_Wisata.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from PIL import Image
+#Logo awal
+image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
+st.image(image)
+st.markdown("<h1 style='text-align: center; color: white;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
+
+st.write("----")
+
+
+#Menu/tabs
+tab1, tab2, tab3 = st.tabs(["Sejarah", "Data Penduduk","Destinasi Wisata"])
+
+#Sejarah
+tab1.subheader("Sejarah Bumi Serumpun Sebalai")
+
+
+#data
+tab2.subheader("Data penduduk ")
+tab2.write(data)
+
+#wisata
+tab3.subheader("Destinasi Wisata")
+tab3.write(data)
--- Web_Dev/Pages/Sejarah.py
+++ Web_Dev/Pages/Sejarah.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from PIL import Image
+#Logo awal
+image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
+st.image(image)
+st.markdown("<h1 style='text-align: center; color: white;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
+
+st.write("----")
+
+
+#Sejarah
+st.subheader("Sejarah Bumi Serumpun Sebalai")
+st.write('Pelan tapi pasti, Bangka Belitung terus bersolek. Kecantikannya tak hanya dirasakan penduduk lokal. Secara nasional, termasuk dunia, juga turut merasakan pesonanya. Ini jelas kebanggaan, juga prestasi, mengingat Bangka Belitung merupakan provinsi baru, terbentuk pada tahun 2000.')
+st.write('Provinsi Kepulauan Bangka Belitung terdiri dari dua pulau besar, yakni Pulau Bangka dan Pulau Belitung. Ada juga pulau-pulau kecil lainnya. Di zaman kerajaan, wilayah ini masuk dalam kekuasaan Sriwijaya, Majahapit, juga Mataram.ru')
 
