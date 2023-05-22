c3066c8b968ebaed65869531aa35e7d0dbf1fddc
Devinchand <100545304+Devinchand@users.noreply.github.com>
2023-05-22 22:58:25 +0700

    Add files via upload

--- Web_Dev/Home.py
+++ Web_Dev/Home.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+from PIL import Image
+
+st.set_page_config(
+    page_title="WebDev"
+)
+
+#Logo awal
+image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
+st.image(image)
+st.markdown("<h1 style='text-align: center; color: white;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
+
+st.write("----")
+
+
+
+
+st.sidebar.success("pilih halaman")
