380c484ebbe7f7a98067273c4a1b74a4b621b10f
Devinchand <100545304+Devinchand@users.noreply.github.com>
2023-05-23 21:43:28 +0700

    Update Data_Penduduk.py

--- Web_Dev/Pages/Data_Penduduk.py
+++ Web_Dev/Pages/Data_Penduduk.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from gsheetsdb import connect
 from PIL import Image
 #Logo awal
 image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
@@ -15,11 +16,20 @@ st.write("----")
 tab1, tab2 = st.tabs(["Data penduduk", "Data Wisatawan"])
 
 #Sejarah
-tab1.subheader("Data Penduduk")
+with tab1 :
+    st.header("Data Penduduk")
+    sheet_id = "1s1MSs8-bu_KIgbC33mVNy_zrlBnSPhD6YR91qW0sHJo"
+    sheet_name = "datapenduduk"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    datapenduduk = pd.read_csv(url)
 
 
 #data
-tab2.subheader("Data Wisatawan")
-
+with tab2 :
+    st.header("Data Wisatawan")
+    sheet_id = "1s1MSs8-bu_KIgbC33mVNy_zrlBnSPhD6YR91qW0sHJo"
+    sheet_name = "dataturis"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    dataturis = pd.read_csv(url)
 
 
