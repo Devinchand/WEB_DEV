702f7c3bfee8fe2fc7eba997a774a07da2e7addc
Devinchand <100545304+Devinchand@users.noreply.github.com>
2023-05-23 09:55:23 +0700

    Update Destinasi_Wisata.py

--- Web_Dev/Pages/Destinasi_Wisata.py
+++ Web_Dev/Pages/Destinasi_Wisata.py
@@ -6,22 +6,19 @@ from PIL import Image
 #Logo awal
 image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
 st.image(image)
-st.markdown("<h1 style='text-align: center; color: white;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; font-size: 30px;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
 
 st.write("----")
 
 
 #Menu/tabs
-tab1, tab2, tab3 = st.tabs(["Sejarah", "Data Penduduk","Destinasi Wisata"])
+tab1, tab2 = st.tabs(["Pulau Bangka", "Pulau Belitung"])
 
 #Sejarah
-tab1.subheader("Sejarah Bumi Serumpun Sebalai")
+tab1.subheader("Pulau bangka")
 
 
 #data
-tab2.subheader("Data penduduk ")
-tab2.write(data)
+tab2.subheader("Pulau Belitung")
 
-#wisata
-tab3.subheader("Destinasi Wisata")
-tab3.write(data)
+
