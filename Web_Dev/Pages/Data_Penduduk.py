3a557ee1260ecd003fb380d25604e49e8af0c9bf
Devinchand <100545304+Devinchand@users.noreply.github.com>
2023-05-23 09:55:48 +0700

    Update Data_Penduduk.py

--- Web_Dev/Pages/Data_Penduduk.py
+++ Web_Dev/Pages/Data_Penduduk.py
@@ -6,22 +6,20 @@ from PIL import Image
 #Logo awal
 image = Image.open("D:\coding\streamlit\WEB_DEV\photo\logobabel.png")
 st.image(image)
-st.markdown("<h1 style='text-align: center; color: white;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; font-size: 30px;'>Bumi Serumpun Sebalai</h1>", unsafe_allow_html=True)
 
 st.write("----")
 
 
 #Menu/tabs
-tab1, tab2, tab3 = st.tabs(["Sejarah", "Data Penduduk","Destinasi Wisata"])
+tab1, tab2 = st.tabs(["Data penduduk", "Data Wisatawan"])
 
 #Sejarah
-tab1.subheader("Sejarah Bumi Serumpun Sebalai")
+tab1.subheader("Data Penduduk")
 
 
 #data
-tab2.subheader("Data penduduk ")
+tab2.subheader("Data Wisatawan")
 
 
-#wisata
-tab3.subheader("Destinasi Wisata")
 
